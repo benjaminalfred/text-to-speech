36e0156e55c95de9743665fe77082689c56d09aa
Ben Martin <benjaminalfred@gmail.com>
2018-10-20 13:58:59 -0700

    added login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,34 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { TodoPage } from '../todo/todo';
+import { RegisterPage } from '../register/register';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
+  templateUrl: 'login.html'
 })
 export class LoginPage {
+  user: any = {
+    email: "",
+    password: ""
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  };
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  ngOnInit() {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  login() {
+    this.navCtrl.push(TodoPage)
   }
+  toRegister() {
+    this.navCtrl.push(RegisterPage);
+  }
+
 
 }
+
+
